b287eaa11e6a315afc77d1564e1e6e45841ce0f1
Zach <spywoawe@gmail.com>
2020-07-31 11:00:02 +1200

    Controls Screen

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    public GameObject MainCanvas;
+    public GameObject ControlsCanvas;
+
     public void PlayGame()
     {
         Time.timeScale = 1;
@@ -14,5 +17,17 @@ public class TitleScreen : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ControlsOn()
+    {
+        MainCanvas.SetActive(false);
+        ControlsCanvas.SetActive(true);
+    }
+
+    public void ControlsOff()
+    {
+        MainCanvas.SetActive(true);
+        ControlsCanvas.SetActive(false);
     }
 }
