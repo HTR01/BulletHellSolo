bfa59a751e21b341d5fc7c60be4bc55225b5ef75
Zach <spywoawe@gmail.com>
2020-08-06 10:12:29 +1200

    Easter Egg + Couple changes

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour
+{
+    bool isEnabled;
+    public GameObject eventSys;
+    public GameObject player;
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Slash))
+        {
+            if (isEnabled == true)
+            {
+                Deactivate();
+            }
+            else
+            {
+                Activate();
+            }
+        }
+    }
+
+    public void Activate()
+    {
+        player.GetComponent<PlayerMovement>().enabled = true;
+        player.GetComponent<Shooting>().enabled = true;
+        eventSys.SetActive(false);
+        isEnabled = true;
+    }
+
+    public void Deactivate()
+    {
+        player.GetComponent<PlayerMovement>().enabled = false;
+        player.GetComponent<Shooting>().enabled = false;
+        eventSys.SetActive(true);
+        isEnabled = false;
+    }
+}
