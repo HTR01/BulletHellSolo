b5c893cba56614a20724b005f794d43c63263288
Zach <spywoawe@gmail.com>
2020-08-11 11:01:07 +1200

    HTR Easter egg

---
+++ Assets/Scripts/HTReasterEgg.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HTReasterEgg : MonoBehaviour
+{
+    int easterEggValue = 0;
+    public Text highScore;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H) && easterEggValue == 0)
+        {
+            easterEggValue = 1;
+            Debug.Log(easterEggValue);
+        }
+        if (Input.GetKeyDown(KeyCode.T) && easterEggValue == 1)
+        {
+            easterEggValue = 2;
+            Debug.Log(easterEggValue);
+        }
+        if (Input.GetKeyDown(KeyCode.R) && easterEggValue == 2)
+        {
+            easterEggValue = 3;
+            Debug.Log(easterEggValue);
+        }
+        if (easterEggValue == 3)
+        {
+            highScore.text = "Created by HTR[01]. Twitter: @HitariAi.";
+        }
+    }
+}
